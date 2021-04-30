60597364b036b49ad1221a99bcd6a66ae21b5ce4
Justin <justinkollie890@yahoo.com>
2021-04-30 13:53:52 -0400

    Added a random colors to div

---
+++ app.js
@@ -0,0 +1,20 @@
+console.log('Script File Connected...');
+
+let container = document.getElementById('container');
+
+// let randomColor = Math.floor(Math.random() * 16777215).toString();
+
+
+for (let i = 0; i < 16 * 16; i++) {
+    let div = document.createElement('div');
+    div.addEventListener('mouseover', (e) => {
+        // let hoverDiv = e.target;
+            // hoverDiv.style.backgroundColor = '#' + randomColor;
+        const randomR = Math.floor(Math.random() * 256);
+        const randomG = Math.floor(Math.random() * 256);
+        const randomB = Math.floor(Math.random() * 256);
+        e.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`;
+    });
+    container.appendChild(div);
+}
+
